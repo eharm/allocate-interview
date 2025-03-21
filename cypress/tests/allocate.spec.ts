27b912301e47865ec8a277399d4c8caca44a0143
Evan Harmony <evan_harmony@hotmail.com>
2025-03-20 23:06:11 -0400

    updated the first test to also add a new todo before submitting the form. Also created a new test confirming the app would not allow submission while todos were left unchecked

--- cypress/tests/allocate.spec.ts
+++ cypress/tests/allocate.spec.ts
@@ -11,7 +11,8 @@ describe('allocate interview tests', () => {
         cy.login();
     })
 
-    it('login and complete default todos', () => {
+    it('Create a todo then check all items and submit', () => {
+        // Check all default todos
         cy.wait('@todos')
             .its('response.body')
             .then((body: todo[]) => {
@@ -30,10 +31,39 @@ describe('allocate interview tests', () => {
                         cy.wrap($el).check({ force: true });
                     })
             })
+
+        // Create a new todo and check it
+        const myTodo = 'create a todo';
+        cy.get('input[type="text"]').type(myTodo + '{enter}');
+        cy.get('input[type="checkbox"]')
+            .last()
+            .should('not.be.checked')
+            .as('myTodo')
+            .siblings('label')
+            .should('have.text', myTodo);
+
+        cy.get('@myTodo').check({ force: true });
+        cy.get('@myTodo').should('be.checked');
         
         cy.contains('button', /submit/i).click();
         cy.location('pathname').should('eq', '/success');
         cy.get('h1').should('contain.text', 'Success!')
+    })
+
+    it('Confirm inability to submit without checking all todos', () => {
+        // Loop through all checkboxes and confirm failed submission after each attempt
+        cy.get('input[type="checkbox"]')
+            .should('have.length', 5)
+            .each(($el, i, $elArr) => {
+                // skip the last iteration
+                if (($elArr.length - 1) === i) return false;
+                cy.wrap($el)
+                    .should('not.be.checked')
+                    .check({ force: true });
+                cy.wrap($el).should('be.checked');
+                cy.contains('button', /submit/i).click();
+                cy.get('.error').should('be.visible');
+            })
     })
 
     afterEach(() => {
